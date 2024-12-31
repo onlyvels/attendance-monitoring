073ab8903ff6e42a219c096d5ecdf49323ae7eee
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-12-31 14:21:28 +0530

    fix: migrate first then cache dockerentrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,11 +2,11 @@
 set -eo pipefail
 shopt -s nullglob
 
+php artisan migrate --isolated --force
 php artisan optimize
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan view:cache
-php artisan migrate --isolated --force
 
 exec "$@"
